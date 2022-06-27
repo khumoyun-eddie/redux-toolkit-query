f10567a96ea1088f4ab2836c9f3e3a715ca3bd64
Khumoyun(Eddie) <78129699+khumoyun-eddie@users.noreply.github.com>
2022-06-28 00:33:39 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCartData, sendCartData } from "./store/cart-actions";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
 
-function App() {
-  const { cartVisible } = useSelector((state) => state.ui);
+// let isInitial = true;
+const App = () => {
+  const [isInitial, setIsInitial] = useState(true);
+  const { cartVisible, notification } = useSelector((state) => state.ui);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      setIsInitial(false);
+      // isInitial = false;
+      return;
+    }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [dispatch, cart]);
 
   return (
-    <Layout>
-      {cartVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/store/cart-actions.js
@@ -0,0 +1,81 @@
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data"
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://redux-query-54b67-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart)
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully"
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed"
+        })
+      );
+    }
+  };
+};
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://redux-query-54b67-default-rtdb.firebaseio.com/cart.json"
+      );
+      if (!response.ok) throw new Error("Could not fetch cart data");
+
+      const data = response.json();
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      // console.log(cartData);
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Fetching cart data failed"
+        })
+      );
+    }
+  };
+};
